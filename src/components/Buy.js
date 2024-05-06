1e2fe831e42028a0717cab686052ee3345772f80
Jiahuita <jl11844@nyu.edu>
2024-05-05 23:03:16 -0400

    added chart to website

--- src/components/Buy.js
+++ src/components/Buy.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import BuyAkuma from '../assets/coinM.jpeg';
 
 function Buy() {
   return (
-      <section id="buy" className="buy">
-        <h2>Buy Now</h2>
-        <a href="https://www.dextools.io/app/en/solana/pair-explorer/BbTw264achKLYdsFBv6y43V38BuKUBnXasRZKVgpgpqq?t=1714916106297" target="_blank" rel="noopener noreferrer" className="buy-link">
-          <img src={BuyAkuma} alt="Buy Akuma" className="buy-image" />
-        </a>
-</section>
+    <section id="buy" className="buy">
+      <h2>Buy Now</h2>
+      <div className="dexscreener-embed">
+        <iframe
+          src="https://dexscreener.com/solana/BbTw264achKLYdsFBv6y43V38BuKUBnXasRZKVgpgpqq?embed=1&theme=dark&info=0"
+          title="DexScreener Chart"
+          width="90%"
+          height="100%"
+          frameBorder="0"
+        ></iframe>
+      </div>
+    </section>
   );
 }
 
